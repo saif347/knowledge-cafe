a4f8ada374e081b794a4cd3f8fd4172e247060da
saif347 <skhwtsaif@gmail.com>
2023-12-25 23:02:00 +0600

    project is building

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './component/header/header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header></Header>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/component/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3>Knowledge Cafe</h3>
+            
+        </div>
+    );
+};
+
+export default Header;
